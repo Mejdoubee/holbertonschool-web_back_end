10ba3b5d03b677abc5585bfd15c4b26f349e71ec
mejdoubee <6235@holbertonstudents.com>
2023-12-03 01:50:16 +0100

    Back_end

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,43 +1,43 @@
 export default class HolbertonCourse {
-	constructor(name, length, students) {
-		this._validateAttributes(name, length, students);
-		this._name = name;
-		this._length = length;
-		this._students = students;
-	}
+    constructor(name, length, students) {
+        this._validateAttributes(name, length, students);
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
 
-	_validateAttributes(name, length, students) {
-		if (typeof name !== 'string') throw new TypeError('Name must be a string');
-		if (typeof length !== 'number') throw new TypeError('Length must be a number');
-		if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
-			throw new TypeError('Students must be an array of strings');
-		}
-	}
+    _validateAttributes(name, length, students) {
+        if (typeof name !== 'string') throw new TypeError('Name must be a string');
+        if (typeof length !== 'number') throw new TypeError('Length must be a number');
+        if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+    }
 
-	get name() {
-		return this._name;
-	}
+    get name() {
+        return this._name;
+    }
 
-	set name(newName) {
-		this._validateAttributes(newName, this._length, this._students);
-		this._name = newName;
-	}
+    set name(newName) {
+        this._validateAttributes(newName, this._length, this._students);
+        this._name = newName;
+    }
 
-	get length() {
-		return this._length;
-	}
+    get length() {
+        return this._length;
+    }
 
-	set length(newLength) {
-		this._validateAttributes(this._name, newLength, this._students);
-		this._length = newLength;
-	}
+    set length(newLength) {
+        this._validateAttributes(this._name, newLength, this._students);
+        this._length = newLength;
+    }
 
-	get students() {
-		return this._students;
-	}
+    get students() {
+        return this._students;
+    }
 
-	set students(newStudents) {
-		this._validateAttributes(this._name, this._length, newStudents)
-		this._students = newStudents;
-	}
+    set students(newStudents) {
+        this._validateAttributes(this._name, this._length, newStudents)
+        this._students = newStudents;
+    }
 }
