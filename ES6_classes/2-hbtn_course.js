a94265ecac08618e8becb846a34d2c389fd53852
mejdoubee <6235@holbertonstudents.com>
2023-12-03 02:42:48 +0100

    Getter and setter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -6,7 +6,7 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-  this _validateAttributes(name, length, students) {
+  _validateAttributes(name, length, students) {
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
     if (typeof length !== 'number') throw new TypeError('Length must be a number');
     if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
